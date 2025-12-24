2be32218f4814edd2f8be95754697a21d279f576
abaciarda <ardaabaci19@gmail.com>
2025-12-24 19:19:47 +0300

    fix withdraw method in SavingsAccount model

--- src/main/java/abaciarda/bankingsystem/models/SavingsAccount.java
+++ src/main/java/abaciarda/bankingsystem/models/SavingsAccount.java
@@ -22,25 +22,31 @@ public class SavingsAccount extends Account {
             return new AccountOperationResponse(false, "Çekmek istediğiniz para miktarı 0'dan büyük olmalı.");
         }
 
-        double finalAmount = amount;
-
         boolean isBeforeMaturity = Instant.now().toEpochMilli() < maturityDate;
+
+        double finalAmount = amount;
+        double interest = 0.0;
 
         if (isBeforeMaturity) {
             double penalty = amount * (EARLY_WITHDRAW_PENALTY_RATE / 100);
             finalAmount += penalty;
         } else {
-            double interest = balance * (interestRate / 100);
-            balance += interest;
+            interest = balance * (interestRate / 100);
         }
 
-        if (finalAmount > balance) {
+        double availableBalance = balance + interest;
+
+        if (finalAmount > availableBalance) {
             return new AccountOperationResponse(false, "Çekmek istediğiniz miktar bakiyenizden daha büyük olamaz.");
+        }
+
+        if (!isBeforeMaturity) {
+            balance += interest;
         }
 
         balance -= finalAmount;
 
         return new AccountOperationResponse(true, isBeforeMaturity ? "Para çekme işlemi vade dolmadan yapıldığı için " + EARLY_WITHDRAW_PENALTY_RATE + "% kesinti ile uygulandı." : "Para çekme işlemi gerçekleşti. Vade sonu gerçekleştiği için faiz miktarı hesabınıza yatırıldı.");
-
     }
+
 }
